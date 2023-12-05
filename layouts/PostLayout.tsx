df31a436dcfc1363b88cc0d0eafcc09d91dddb2b
Orc Hwang <orchwang@Orcui-MacBookAir.local>
2023-12-05 12:59:44 +0900

    Update giscus settings

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -12,7 +12,7 @@ import ScrollTopAndComment from '@/components/ScrollTopAndComment'
 
 const editUrl = (path) => `${siteMetadata.siteRepo}/blob/main/data/${path}`
 const discussUrl = (path) =>
-  `https://mobile.twitter.com/search?q=${encodeURIComponent(`${siteMetadata.siteUrl}/${path}`)}`
+  `https://twitter.com/search?q=${encodeURIComponent(`${siteMetadata.siteUrl}/${path}`)}`
 
 const postDateTemplate: Intl.DateTimeFormatOptions = {
   weekday: 'long',
